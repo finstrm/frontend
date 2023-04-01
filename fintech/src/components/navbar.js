30b80ce27922fbfb6381e7546d5379781f5b52c8
Pranav Ramesh <laphatize@protonmail.com>
2023-04-01 14:44:39 -0400

    RRR

---
+++ fintech/src/components/navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid flex-1">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                </div>
+            </nav>
+        </>
+    );
+}
